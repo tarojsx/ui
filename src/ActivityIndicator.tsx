dcaeb64a4c702ace0cd0ca3a8c0a215e31dc47f7
colder <colder@vitarn.com>
2020-06-09 07:22:07 +0800

    fix: 传递 style

--- src/ActivityIndicator.tsx
+++ src/ActivityIndicator.tsx
@@ -8,10 +8,11 @@ import '../style/ActivityIndicator.scss'
 
 export interface ActivityIndicatorProps extends AtActivityIndicatorProps {
     isOpened?: boolean
+    style?: React.CSSProperties
 }
 
 export const ActivityIndicator: React.FC<ActivityIndicatorProps> = props => {
-    const { className, color, size, mode, content, isOpened = true, children } = props
+    const { className, style, color, size, mode, content, isOpened = true, children } = props
 
     const rootClass = classNames(
         'at-activity-indicator',
@@ -23,7 +24,7 @@ export const ActivityIndicator: React.FC<ActivityIndicatorProps> = props => {
     )
 
     return (
-        <View className={rootClass}>
+        <View className={rootClass} style={style}>
             <View className="at-activity-indicator__body">
                 <Loading size={size} color={color} />
             </View>
